02a40f8b884e408a26f992f6bb5a4fd94e7cd71e
Łukasz Matysik <lukasz.matysik6@gmail.com>
2019-04-10 13:32:33 +0200

    Copying paint classes to enable paint working

---
+++ app/src/main/java/com/swistak/androidpaint/FingerPath.kt
@@ -0,0 +1,6 @@
+package com.swistak.androidpaint
+
+
+import android.graphics.Path
+
+class FingerPath(var color: Int, var emboss: Boolean, var blur: Boolean, var strokeWidth: Int, var path: Path)
--- app/src/main/java/com/swistak/androidpaint/MainActivity.kt
+++ app/src/main/java/com/swistak/androidpaint/MainActivity.kt
@@ -2,11 +2,59 @@ package com.swistak.androidpaint
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val paint = paintView
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+        paint.init(metrics)
+        Log.d("kappa","Clear button clicked")
+        print("alfa i omega")
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.top_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.normal -> {
+                paintView.normal()
+                return true
+            }
+            R.id.emboss -> {
+                paintView.emboss()
+                return true
+            }
+            R.id.blur -> {
+                paintView.blur()
+                return true
+            }
+            R.id.clear -> {
+                Log.d("kappa","Clear button clicked")
+                paintView.clear()
+                return true
+            }
+            R.id.undo ->{
+                paintView.undo()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
